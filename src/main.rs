39333377f08da6255015d4cbb3c9148ee86a52f8
Hideki A. Ikeda <hidekiai@CodeMonkeyNinja.dev@users.noreply.github.com>
2021-07-25 20:21:44 -0500

    Commented and/or remoted unused code, still have warnings but will get to it when time is avail

--- src/main.rs
+++ src/main.rs
@@ -26,50 +26,6 @@
 // There is an optimized version which does not call math-libs, but for this
 // port, it would be more straight-forward to use the one  with cos/sin libs
 //
-// int main() {
-//     float A = 0, B = 0;
-//     float i, j;
-//     int k;
-//     float z[1760];
-//     char b[1760];
-//     printf("\x1b[2J");
-//     for(;;) {
-//         memset(b,32,1760);
-//         memset(z,0,7040);
-//         for(j=0; j < 6.28; j += 0.07) {
-//             for(i=0; i < 6.28; i += 0.02) {
-//                 float c = sin(i);
-//                 float l = cos(i);
-//                 float d = cos(j);
-//                 float f = sin(j);
-//                 float e = sin(A);
-//                 float g = cos(A);
-//                 float m = cos(B);
-//                 float n = sin(B);
-//
-//                 float h = d + 2;
-//                 float D = 1 / (c * h * e + f * g + 5);
-//                 float t = c * h * g - f * e;
-//                 int x = 40 + 30 * D * (l * h * m - t * n);
-//                 int y = 12 + 15 * D * (l * h * n + t * m);
-//                 int o = x + 80 * y;
-//                 int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
-//                 if(22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
-//                     z[o] = D;
-//                     b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
-//                 }
-//             }
-//         }
-//         printf("\x1b[H");
-//         for(k = 0; k < 1761; k++) {
-//             putchar(k % 80 ? b[k] : 10);
-//             A += 0.00004;
-//             B += 0.00002;
-//         }
-//         usleep(30000);
-//     }
-//     return 0;
-// }
 
 //use std::ptr;
 use std::{thread, time}; // sleep
@@ -223,25 +179,25 @@ fn main() {
                 let luminance_index = 8.0 * luminance; // range it between 0..11
                 let depth = mut_z_buff[us_pos_xy];
 
-                // calculate luminance. ugly, but correct.
-                if screen_height > projection_y
-                    && projection_y > 0.0
-                    && projection_x > 0.0
-                    && screen_width > projection_x
-                    && one_over_z > depth
-                // test against the z-buffer.  larger 1/z means the pixel is closer to the viewer than what's already plotted.
-                {
-                    //mut_z_buff[us_pos_xy] = one_over_z;
-                    let index = match luminance_index {
-                        _ if luminance_index > 0.0 => luminance_index as usize,
-                        _ => 0 as usize, // no light
-                    };
-                    //mut_render_buff[us_pos_xy] = pixels[index];
-                }
-                //else {
-                //    // until we get clear buffer working, we'll just fill in the array...
-                //    mut_render_buff[us_pos_xy] = char_space;
+                //// calculate luminance. ugly, but correct.
+                //if screen_height > projection_y
+                //    && projection_y > 0.0
+                //    && projection_x > 0.0
+                //    && screen_width > projection_x
+                //    && one_over_z > depth
+                //// test against the z-buffer.  larger 1/z means the pixel is closer to the viewer than what's already plotted.
+                //{
+                //    //mut_z_buff[us_pos_xy] = one_over_z;
+                //    let index = match luminance_index {
+                //        _ if luminance_index > 0.0 => luminance_index as usize,
+                //        _ => 0 as usize, // no light
+                //    };
+                //    //mut_render_buff[us_pos_xy] = pixels[index];
                 //}
+                ////else {
+                ////    // until we get clear buffer working, we'll just fill in the array...
+                ////    mut_render_buff[us_pos_xy] = char_space;
+                ////}
 
                 // int o = x + 80 * y;
                 // int N = 8 * ((sin_theta * sin_a - sin_phi * cos_theta * cos_a) * cos_b - sin_phi * cos_theta * sin_a - sin_theta * cos_a - cos_phi * cos_theta * sin_b);     // luminance_index is now in the range 0..11 (8*sqrt(2) = 11.3), now we lookup the character corresponding to the luminance and plot it in our output:
